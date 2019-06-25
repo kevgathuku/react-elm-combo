4408f3ef7b2b9e959418a9cf72f44723083a586f
Kevin Ndung'u <kevgathuku@gmail.com>
2019-06-25 09:33:45 +0300

    Add config-overrides.js

---
+++ config-overrides.js
@@ -0,0 +1,7 @@
+/* config-overrides.js */
+
+// Override the Webpack config to use when compiling your react app
+module.exports = function override(config, env) {
+  //do stuff with the webpack config...
+  return config;
+}
